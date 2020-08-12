7c896006d0535cb09a0bb6768d0810f79fdd05e7
SibTrox <enzonicolascorrea@gmail.com>
2020-08-12 20:19:10 -0300

    Añadiendo imagenes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,16 @@
-		document.addEventListener('DOMContentLoaded', function() {
-		    var elems = document.querySelectorAll('.carousel');
-		    var instance = M.Carousel.init(elems,{
-		    	duration: 300,
-		    	fullWidth: true,
-		    	indicators: true
-		    });
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.carousel');
+    var instance = M.Carousel.init(elems,{
+		duration: 300,
+		fullWidth: true,
+		indicators: true
+	});
 
-			var elemsMedia = document.querySelectorAll('.materialboxed');
-   			var instances = M.Materialbox.init(elemsMedia);
-		});
+	var elemsMedia = document.querySelectorAll('.materialboxed');
+	var instances = M.Materialbox.init(elemsMedia);
 
+	var elem = document.querySelector('.collapsible.expandable');
+	var instance = M.Collapsible.init(elem, {
+		accordion: false
+	});
+});
