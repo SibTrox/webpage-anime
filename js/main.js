a7fb5bf864595184ef6fda4a580488e6edf6ae37
SibTrox <enzonicolascorrea@gmail.com>
2020-08-15 21:42:44 -0300

    Cambio de background con footer

--- js/main.js
+++ js/main.js
@@ -9,8 +9,21 @@ document.addEventListener('DOMContentLoaded', function() {
 	var elemsMedia = document.querySelectorAll('.materialboxed');
 	var instances = M.Materialbox.init(elemsMedia);
 
-	var elem = document.querySelector('.collapsible.expandable');
-	var instance = M.Collapsible.init(elem, {
-		accordion: false
-	});
+	var lainFooter = document.getElementById("lain-footer");
+	lainFooter.onclick = function(background){
+		var background = ["bg_main1", "bg_main2", "bg_main3", "bg_main4", "bg_main5"];
+		var body = document.getElementById("body-page");
+		for (var i = 0; i < background.length; i++) {
+			if(body.className == background[i]){
+				if(i == 4){
+					body.className = "bg_main1";
+					break
+				}else{
+					console.log(background[i+1])
+					body.className = background[i+1];
+					break
+				}
+			}
+		 }
+	}
 });
